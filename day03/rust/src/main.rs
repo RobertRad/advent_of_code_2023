3da9a810e6a46652745fb75d1f2ced60e91f047d
Robert <robert>
2023-12-20 09:08:09 +0100

    Day03 - Part2 - Rust

--- day03/rust/src/main.rs
+++ day03/rust/src/main.rs
@@ -1,10 +1,16 @@
+use std::collections::HashMap;
 use std::fs;
 
 fn main() {
-    let contents = fs::read_to_string("../test.txt")
+    let contents = fs::read_to_string("../input.txt")
         .expect("Should have been able to read the file");
 
     let lines: Vec<&str> = contents.lines().collect();
+    part1(&lines);
+    part2(&lines);
+}
+
+fn part1(lines: &Vec<&str>) {
     let mut sum: u32 = 0;
 
     for (index, line) in lines.iter().enumerate() {
@@ -38,14 +44,7 @@ fn main() {
             };
         }
     }
-    println!("Sum: {sum}");
-}
-
-#[derive(Debug)]
-struct FoundNumber {
-    number: u32,
-    start_index: usize,
-    end_index: usize,
+    println!("Part1: {sum}");
 }
 
 fn find_number(line: &str, start_at: usize) -> Option<FoundNumber> {
@@ -71,4 +70,92 @@ fn has_symbol(line: &str, start_index: usize, end_index: usize) -> bool {
     let sub = &line[start_index..end_index];
     let result = sub.contains(|c: char| !c.is_numeric() && c != '.' );
     result
+}
+
+#[derive(Debug)]
+struct FoundNumber {
+    number: u32,
+    start_index: usize,
+    end_index: usize,
+}
+
+fn part2(lines: &Vec<&str>) {
+    let mut sum: u32 = 0;
+    let mut found_gears: HashMap<CharWithPosition, u32> = HashMap::new();
+
+    for (index, line) in lines.iter().enumerate() {
+        let mut start_at = 0;
+        loop {
+            let number = find_number(line, start_at);
+            match number {
+                None => break,
+                Some(found_number) => {
+                    start_at = found_number.end_index;
+                    let number = found_number.number;
+                    let neighbour_symbol = get_neighbour_symbol(found_number, line, index, lines);
+                    // println!("Line [{index}]: {line} - {number}, {:?}", neighbour_symbol);
+                    let neighbour_symbol = neighbour_symbol.and_then(|s| if s.c == '*' { Some(s) } else { None });
+                    match neighbour_symbol {
+                        None => {},
+                        Some(neighbour_symbol) => {
+                            let previous_number = found_gears.get(&neighbour_symbol);
+                            match previous_number {
+                                None => {
+                                    found_gears.insert(neighbour_symbol, number);
+                                },
+                                Some(previous_number) => {
+                                    sum += number * previous_number;
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+        }
+    }
+    println!("Part2: {sum}");
+}
+
+#[derive(Debug)]
+#[derive(PartialEq)]
+#[derive(Eq)]
+#[derive(Hash)]
+struct CharWithPosition {
+    c: char,
+    x: usize,
+    y: usize,
+}
+
+fn get_neighbour_symbol(number: FoundNumber, line: &str, y: usize, lines: &Vec<&str>) -> Option<CharWithPosition> {
+    let symbol_before = if number.start_index == 0 { None } else { line.chars().nth(number.start_index - 1) };
+    let symbol_before = if symbol_before == Some('.') { None } else { symbol_before };
+    let symbol_before = symbol_before.map(|c| CharWithPosition{c, x: number.start_index - 1, y} );
+    let symbol_after = if number.end_index == line.len() { None } else { line.chars().nth(number.end_index) };
+    let symbol_after = if symbol_after == Some('.') { None } else { symbol_after };
+    let symbol_after = symbol_after.map(|c| CharWithPosition{c, x: number.end_index, y} );
+    let previous_line = if y > 0 { Some(lines[y - 1]) } else { None };
+    let symbol_in_previous_line = match previous_line {
+        None => None,
+        Some(previous_line) => find_symbol(previous_line, y - 1, number.start_index, number.end_index),
+    };
+    let next_line = if y < lines.len() - 1 { Some(lines[y + 1]) } else { None };
+    let symbol_in_next_line = match next_line {
+        None => None,
+        Some(next_line) => find_symbol(next_line, y + 1, number.start_index, number.end_index),
+    };
+    symbol_before.or(symbol_after).or(symbol_in_previous_line).or(symbol_in_next_line)
+}
+
+fn find_symbol(line: &str, y: usize, start_index: usize, end_index: usize) -> Option<CharWithPosition> {
+    let start_index = if start_index == 0 { 0 } else { start_index - 1};
+    let end_index = if end_index == line.len() { end_index } else { end_index + 1};
+    let sub = &line[start_index..end_index];
+    let symbol_index = sub.find(|c: char| !c.is_numeric() && c != '.');
+    match symbol_index {
+        None => None,
+        Some(symbol_index) => {
+            let c = sub.chars().nth(symbol_index).unwrap();
+            Some(CharWithPosition { c, x: start_index + symbol_index, y })
+        },
+    }
 }
