2f173f4068000fc4b693b2b2ffd2a8bfa9a73d6e
Robert Kindermann <robert.kindermann@alphabrik.com>
2023-12-21 20:08:51 +0100

    Day04 - Part1 - Rust

--- day04/rust/src/main.rs
+++ day04/rust/src/main.rs
@@ -1,7 +1,9 @@
+use regex::Regex;
+use std::collections::BTreeSet;
 use std::fs;
 
 fn main() {
-    let contents = fs::read_to_string("../test.txt")
+    let contents = fs::read_to_string("../input.txt")
         .expect("Should have been able to read the file");
 
     let lines: Vec<&str> = contents.lines().collect();
@@ -9,7 +11,32 @@ fn main() {
 }
 
 fn part1(lines: &Vec<&str>) {
-    for (index, line) in lines.iter().enumerate() {
-        println!("Line [{index}]: {line}");
+    let mut sum: u32 = 0;
+    let start_regex = Regex::new("Card +\\d+: ").unwrap();
+    for line in lines {
+        let begin_content = start_regex.find(line).unwrap().end();
+        let splitted = line[begin_content..].split("|").collect::<Vec<&str>>();
+
+        fn extract_numbers(string: &Vec<&str>, index: usize) -> Vec<u32> {
+            let numbers = string.get(index).unwrap();
+            let numbers: Vec<&str> = numbers.split_whitespace().collect();
+            let numbers: Vec<u32> = numbers.iter().map(|num| num.parse::<u32>().unwrap()).collect();
+            numbers
+        }
+
+        let winning_numbers = extract_numbers(&splitted, 0);
+        let my_numbers = extract_numbers(&splitted, 1);
+        // println!("winning_numbers: {:?}", winning_numbers);
+        // println!("my_numbers: {:?}", my_numbers);
+        let winning_numbers: BTreeSet<u32>= BTreeSet::from_iter(winning_numbers.iter().cloned());
+        let mut match_count = 0;
+        for number in my_numbers {
+            if winning_numbers.contains(&number) {
+                match_count += 1;
+            }
+        }
+        let value = if match_count == 0 { 0 } else { 2_u32.pow(match_count - 1) };
+        sum += value;
     }
+    println!("Part1: {sum}");
 }
